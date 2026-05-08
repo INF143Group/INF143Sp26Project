8bd2d64cdecc6fe615697a38895f6683f57dfaeb
Gilberto Froylan Fernandez <gilberff@uci.edu>
2026-05-08 14:41:27 -0700

    Added event popup when clicking add event today

--- Components/Calendar.jsx
+++ Components/Calendar.jsx
@@ -1,15 +1,30 @@
+import { useRef, useState } from "react";
 import NavBar from "../Components/nav-bar.jsx";
 import  FullCalendar  from "@fullcalendar/react";
 import  dayGridPlugin  from "@fullcalendar/daygrid";
 import  interactionPlugin   from "@fullcalendar/interaction";
 
 import "../Styles/Calendar.css";
+import EventAddPopup from "./EventAddPopup.jsx";
 
 const handleDateClick = (arg) => {
     alert(arg.dateStr)
   }
 
 export default function Calendar(){
+
+    const calendarRef = useRef(null);
+    const eventPopupRef = useRef(null);
+
+    const [isSchedulerOpen, setIsSchedulerOpen] = useState(true);
+
+    function displayAddEventDiv(arg){
+        setIsSchedulerOpen(true);
+    }
+    function hideAddEventDiv(arg){
+        setIsSchedulerOpen(false);
+    }
+
     return(
         <div className="calendar-wrapper">
             <div className={"div1"} id={"nav-bar"}>
@@ -17,27 +32,36 @@ export default function Calendar(){
             </div>
             <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.20/index.global.min.js"></script>
             <h1>Calendar</h1>
-            <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                dateClick={handleDateClick}
-                customButtons={{
-                    addEventTodayButton: {
-                        text: 'add an event today',
-                        click: handleSchedulerButtonClick
-                    }
-                }}
-                headerToolbar= {{
-                    center: 'addEventTodayButton'
-                }}
-            />
+            <div className="stack">
+                <FullCalendar
+                    ref={calendarRef}
+                    plugins={[dayGridPlugin, interactionPlugin]}
+                    initialView="dayGridMonth"
+                    dateClick={handleDateClick}
+                    customButtons={{
+                        addEventTodayButton: {
+                            text: 'add an event today',
+                            click: displayAddEventDiv
+                        }
+                    }}
+                    headerToolbar= {{
+                        center: 'addEventTodayButton'
+                    }}
+                />
+                <div className="top-layer">
+                    <EventAddPopup
+                    isOpen={isSchedulerOpen}
+                    onClose={hideAddEventDiv}
+                    />
+                </div>
+                
+            </div>
         </div>
+        
     );
 }
 
-function handleSchedulerButtonClick(){
-    alert("Scheduler button clicked!");
-}
+
 
 function SchedulerDiv(){
     return(

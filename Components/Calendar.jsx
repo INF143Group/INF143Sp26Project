eb0dc289c5b150af7507d7de23f7a5577e3a2210
Gilberto Froylan Fernandez <gilberff@uci.edu>
2026-05-13 15:36:42 -0700

    Added more robust frontend handling, ready for backend work.

--- Components/Calendar.jsx
+++ Components/Calendar.jsx
@@ -1,29 +1,55 @@
 import { useRef, useState } from "react";
+import {ToastContainer, toast} from "react-toastify";
 import NavBar from "../Components/nav-bar.jsx";
 import  FullCalendar  from "@fullcalendar/react";
 import  dayGridPlugin  from "@fullcalendar/daygrid";
 import  interactionPlugin   from "@fullcalendar/interaction";
-
-import "../Styles/Calendar.css";
 import EventAddPopup from "./EventAddPopup.jsx";
 
-const handleDateClick = (arg) => {
-    alert(arg.dateStr)
-  }
+import "../Styles/Calendar.css";
 
 export default function Calendar(){
 
     const calendarRef = useRef(null);
     const eventPopupRef = useRef(null);
 
-    const [isSchedulerOpen, setIsSchedulerOpen] = useState(true);
+    const [isSchedulerOpen, setIsSchedulerOpen] = useState(false);
+
+    function SchedulerDiv(){
+        return(
+            <div className="scheduler-wrapper">
+                <h1>Scheduler</h1>
+            </div>
+        );
+    }
 
     function displayAddEventDiv(arg){
         setIsSchedulerOpen(true);
     }
-    function hideAddEventDiv(arg){
+    function hideAddEventDiv(dateObj){
+        if (dateObj.success){
+            toast.success(() => AddSubtext(dateObj),{
+                className: '!w-fit !max-w-none',
+            });
+        } else{
+            // toast.error("Event addition cancelled.");
+            toast.warn("Not scheduled: " + dateObj.msg);
+        }
         setIsSchedulerOpen(false);
     }
+    function AddSubtext(dateObj){
+        return (
+            <div className="grid grid-cols-[1fr_1px_80px] w-full">
+                <div className="flex flex-col p-3">
+                    <h4 className="text-zinc-800 text-sm font-semibold">Event Submitted</h4>
+                    <p className="m-0 text-sm">Interviewer: <b>{dateObj.interviewerName}</b></p>
+                    <p className="m-0 text-sm whitespace-nowrap">Interviewer email: <b>{dateObj.interviewerEmail}</b></p>
+                    <p className="m-0 text-sm">Date: <b>{dateObj.date}</b></p>
+                    <p className="m-0 text-sm">Time: <b>{dateObj.time}</b></p>
+                </div>
+            </div>
+        )
+    }    
 
     return(
         <div className="calendar-wrapper">
@@ -32,12 +58,15 @@ export default function Calendar(){
             </div>
             <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.20/index.global.min.js"></script>
             <h1>Calendar</h1>
+            <ToastContainer
+                toastStyle={{width: "fit-content", maxWidth: "none"}}
+            />
             <div className="stack">
                 <FullCalendar
                     ref={calendarRef}
                     plugins={[dayGridPlugin, interactionPlugin]}
                     initialView="dayGridMonth"
-                    dateClick={handleDateClick}
+                    dateClick={displayAddEventDiv}
                     customButtons={{
                         addEventTodayButton: {
                             text: 'add an event today',
@@ -54,7 +83,6 @@ export default function Calendar(){
                     onClose={hideAddEventDiv}
                     />
                 </div>
-                
             </div>
         </div>
         
@@ -62,11 +90,4 @@ export default function Calendar(){
 }
 
 
-
-function SchedulerDiv(){
-    return(
-        <div className="scheduler-wrapper">
-            <h1>Scheduler</h1>
-        </div>
-    );
-}
+    

e88b847af06d6d439a0749f08e0e4b1cdb608954
Gilberto Froylan Fernandez <gilberff@uci.edu>
2026-05-19 15:46:17 -0700

    Events are now added to front-end on success

--- Components/Calendar.jsx
+++ Components/Calendar.jsx
@@ -55,6 +55,11 @@ function submitAndHideDiv(dateObj){
         if (status.success===false){
             toast.error("Failed to schedule event.");
         } else {
+            calendarRef.current.getApi().addEvent({
+                title: "Interview with " + dateObj.interviewerName,
+                start: dateObj.date + " " + dateObj.time,
+            })
+            calendarRef.current.getApi().refetchEvents();
             toast.success(() => AddSubtext(dateObj), {
                 className: '!w-fit !max-w-none',
             });

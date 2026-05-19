a99e59452e67206ec0a35e24ee330a6b456c60c4
INF143Group <inf143sp26group@gmail.com>
2026-05-19 15:19:58 -0700

    routed homepage, meet the team page, and about page

--- Components/main.jsx
+++ Components/main.jsx
@@ -7,6 +7,9 @@ import Login from './Login.jsx'
 import Calendar from './Calendar.jsx'
 import Create from './create.tsx'  // ADD THIS
 import Help from './Help.tsx'  // ADD THIS
+import Home from './Home.jsx'
+import Team from './Team.jsx'
+import About from './About.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,6 +20,9 @@ createRoot(document.getElementById('root')).render(
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/create" element={<Create />} />  {/* ADD THIS */}
         <Route path="/help" element={<Help />} />  // ADD THIS
+        <Route path="/home" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,

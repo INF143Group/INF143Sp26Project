28191bea646f1a85a22320c285d59c2a6a35e0a9
INF143Group <inf143sp26group@gmail.com>
2026-05-19 15:22:31 -0700

    updated about on navbar

--- Components/nav-bar.jsx
+++ Components/nav-bar.jsx
@@ -23,7 +23,7 @@ const NavBar = () => {
                         <Nav.Link href="#chat">Chats</Nav.Link>
                         <Nav.Link href="#resources">Resources</Nav.Link>
                         <Nav.Link href="calendar">Calendar</Nav.Link>
-                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link onClick={() => navigate("/about")}>About</Nav.Link>
                         <Nav.Link onClick={() => navigate("/help")}>Help</Nav.Link>
                     </Nav>
 

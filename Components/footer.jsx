a6340e065bfd4d09cfb7b1865f975ac48461d79d
Aidan Roy <agroy1@uci.edu>
2026-05-27 18:25:05 -0700

    Links

--- Components/footer.jsx
+++ Components/footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
         <Container fluid className="flex-column justify-content-center align-items-center py-1 footer-container">
             <span className="footer-copyright">© 2026 - Company, Inc. All rights reserved. UCI</span>
             <Nav className="justify-content-center footer-nav">
-                <NavLink href="#home">Home</NavLink>
-                <NavLink href="#resources">Resources</NavLink>
-                <NavLink href="#about">About</NavLink>
+                <NavLink href="/home">Home</NavLink>
+                <NavLink href="/resources">Resources</NavLink>
+                <NavLink href="/about">About</NavLink>
             </Nav>
         </Container>
     </Navbar>
